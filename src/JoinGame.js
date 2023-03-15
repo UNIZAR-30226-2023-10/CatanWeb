aae590a73a295663fc700c1a716ccd44732fb775
MrLauren24 <75625624+MrLauren24@users.noreply.github.com>
2023-03-15 18:32:38 +0100

    Pagina Join Game creada

---
+++ src/JoinGame.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import logo from './Catan-logo-1.png'
+import './JoinGame.css'
+import './Common.css'
+
+function JoinGame() {
+    return (
+        <div className='JoinGame-header | Common-Header'>
+            <div className='JoinGame-container'>
+                <img src={logo} className="JoinGame-logo" alt="logo" />
+                <form className='JoinGame-form'>
+                    <div className="JoinGame-cover">
+                        <h1>INSERT GAME CODE:</h1>
+                        <input type="text" placeholder="" />
+                        <Link to='/game' className='JoinGame-btn'>
+                                <a className='linkado'>JoinGame</a>
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default JoinGame;
