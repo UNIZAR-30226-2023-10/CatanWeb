04d497fb0471a3916293513a6ff6e40614de540c
MrLauren24 <75625624+MrLauren24@users.noreply.github.com>
2023-03-15 18:31:37 +0100

    Rutas actualizadas

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import Home  from './Home'
 import Login from './Login'
 import Main  from './Main'
 import Game  from './Game'
+import NewGame  from './NewGame'
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
+import Register from './Register'
+import JoinGame from './JoinGame'
 
 function App() {
     return (
@@ -16,9 +19,13 @@ function App() {
                 <Route path="/login" element={<Login />}/> 
                 <Route path="/main"  element={<Main />}/>
                 <Route path="/game"  element={<Game />}/>
+                <Route path="/newGame"  element={<NewGame />}/>
+                <Route path="/register" element={<Register />}/> 
+                <Route path="/joinGame"  element={<JoinGame />}/>
             </Routes>
         </Router>
     );
 }
 
 export default App;
+
