13f6edb49cd89dd296a05394bdd1d3653f6d1483
MrLauren24 <75625624+MrLauren24@users.noreply.github.com>
2023-03-15 18:34:10 +0100

    Main nuevo

--- src/Main.js
+++ src/Main.js
@@ -1,54 +1,30 @@
 import './Common.css'
-import './Main.css'
+import './MainDos.css'
 import {useState} from 'react'
 import {Link} from 'react-router-dom'
 import logo from './Catan-logo-1.png'
 
 function Main() {
-
-    const [content, setContent] = useState("default")
-    const handleClick = (newContent) => {
-        setContent(newContent);
-    };
     return (
         <div className="Main-Header | Common-Header">
-            <img src={logo} className="Main-logo" alt="logo" />
-            <div className="Main-menu">
-                {content === "default" && (
-                    <>
-                        <Link className="Common-button | Main-button" to="/login">
-                            Volver pa' tras
-                        </Link>                
-                        <Link className="Common-button | Main-button" style={{backgroundColor: 'gray', cursor: 'not-allowed'}} to="/main">
-                            Continuar
-                        </Link>                
-                        <Link className="Common-button | Main-button" onClick={() => handleClick("new-game")}>
-                            Nueva partida
-                        </Link>                
-                        <Link className="Common-button | Main-button" style={{backgroundColor: 'gray', cursor: 'not-allowed'}} to="/main">
-                            Unirse a partida
-                        </Link>
-                    </>
-                )}
-                {content === "new-game" && (
-                    <>
-                        <div className="New-game-players">
-                            <div className="New-game-player"></div>
-                            <div className="New-game-player"></div>
-                            <div className="New-game-player"></div>
-                            <div className="New-game-player"></div>
-                        </div>
-                        <div className="New-game-buttons">
-                            <Link className="Common-button + New-game-button" onClick={() => handleClick("default")}>
-                                Volver pa' tras
-                            </Link>
-                            <Link className="Common-button + New-game-button" to="/game">
-                                Empezar
-                            </Link>
-                        </div>
-                    </>
-                )}
-            </div>
+            <div className='Main-container'>
+                <img src={logo} className="Main-logo" alt="logo" />
+                <div className="Main-cover">
+                    <Link to='/login' className='Main-btn'>
+                        <a className='linkado'>Volver pa' tras</a>
+                    </Link>
+                    <Link to='/newGame' className='Main-btn'>
+                        <a className='linkado'>New game</a>
+                    </Link>
+                    <Link to='/' className='Main-btn'>
+                        <a className='linkado'>Continue Game</a>
+                    </Link>
+                    <Link to='/joinGame' className='Main-btn'>
+                        <a className='linkado'>Join Game</a>
+                    </Link>
+
+                </div>
+            </div> 
         </div>
     )
 }
