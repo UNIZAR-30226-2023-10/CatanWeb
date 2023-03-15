ddb375a23b383874dfe6c5aabcad13516a0bb918
Rael167 <75581099+Rael167@users.noreply.github.com>
2023-03-15 16:55:26 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/UNIZAR-30226-2023-10/CatanWeb""
    This reverts commit 4e24f3d0dbb412044c31402c314ebb34a116b63c.

---
+++ src/Register.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import logo from './Catan-logo-1.png'
+import './Register.css'
+import './Common.css'
+
+function Register() {
+    return (
+        <div className='Register-header | Common-Header'>
+            <div className='Register-container'>
+                <img src={logo} className="Register-logo" alt="logo" />
+                <form>
+                <div className="Register-cover">
+                    <h1>Register</h1>
+                    <input type="text" placeholder="EMAIL" />
+                    <input type="text" placeholder="USERNAME" />
+                    <input type="password" placeholder="PASSWORD" />
+                    <input type="password" placeholder="REPEAT PASSWORD" />
+                    <Link to='/login' className='linkado'>
+                        <a>Go back to login</a>
+                    </Link>
+                    <Link to='/main' className='Register-btn'>
+                            <a className='linkado'>Register</a>
+                    </Link>
+                </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
