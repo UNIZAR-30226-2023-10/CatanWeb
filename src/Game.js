3e9c8689a558bb911eb19efcc0ca3040b5e1ffd8
Rael167 <75581099+Rael167@users.noreply.github.com>
2023-03-15 16:52:05 +0100

    update tablero
    aspecto estadisticas de jugadores
    aspecto recursoss del jugador

--- src/Game.js
+++ src/Game.js
@@ -1,80 +1,165 @@
-import {Link} from 'react-router-dom'
 import './Game.css'
 import './Common.css'
 
 function Game() {
     return (
-        <div className='Game-header'>
-            <div className='Game-board'>
-                <div className='Game-board-row'>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
+        <div className="Game-header | Common-Header" >
+            <div className='Game-board-statics'>
+                <div className='Game-board-statics-name'>
+                        USUARIO1&nbsp;&nbsp;&nbsp;&nbsp;Recursos:X
                     </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
+                    <div className='Game-board-statics-name'>
+                        Desarrollos:X&nbsp;Caballeros:X
                     </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
+                    <div className='Game-board-statics-name'>
+                        Carreteras:X&nbsp;Puntos:X
                     </div>
+                </div>  
+            <div className='Game-board-row'>
+            <div className='Game-board-statics'>
+                <div className='Game-board-statics-name'>
+                        USUARIO2&nbsp;&nbsp;&nbsp;&nbsp;Recursos:X
+                    </div>
+                    <div className='Game-board-statics-name'>
+                        Desarrollos:X&nbsp;Caballeros:X
+                    </div>
+                    <div className='Game-board-statics-name'>
+                        Carreteras:X&nbsp;Puntos:X
+                    </div>
+                </div>  
+                <div className='Game-board'>
+                    <div className='Game-board-row'>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                    </div>
+                    <div className='Game-board-row'>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                    </div>
+                    <div className='Game-board-row'>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                    </div>
+                    <div className='Game-board-row'>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                    </div>
+                    <div className='Game-board-row'>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                        <div className='Game-board-cell'>
+                            <div className='Game-board-number'>8</div>
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className='Game-board-row'>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
+                <div className='Game-board-statics'>
+                <div className='Game-board-statics-name'>
+                        USUARIO3&nbsp;&nbsp;&nbsp;&nbsp;Recursos:X
                     </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
+                    <div className='Game-board-statics-name'>
+                        Desarrollos:X&nbsp;Caballeros:X
                     </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
+                    <div className='Game-board-statics-name'>
+                        Carreteras:X&nbsp;Puntos:X
                     </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                </div>
-                <div className='Game-board-row'>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                </div>
-                <div className='Game-board-row'>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                </div>
-                <div className='Game-board-row'>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                    <div className='Game-board-cell'>
-                        <div className='Game-board-number'>8</div>
-                    </div>
-                </div>
+                </div>  
+                
             </div>
+            <div className='Game-board-rec'>
+                <div className='Game-board-resources'>
+                X
+                    <div className='Game-board-resources-name'>Madera</div>
+                </div>
+                <div className='Game-board-resources'>
+                X
+                    <div className='Game-board-resources-name'>Trigo</div>
+                </div>
+                <div className='Game-board-resources'>
+                X
+                    <div className='Game-board-resources-name'>Barro</div>
+                </div>
+                <div className='Game-board-resources'>
+                    X
+                    <div className='Game-board-resources-name'>Oveja</div>
+                </div>
+                <div className='Game-board-resources'>
+                X
+                    <div className='Game-board-resources-name'>Piedra</div>
+                </div> 
+                <div className='Game-board-statics'>
+                <div className='Game-board-statics-name'>
+                        USUARIO2&nbsp;&nbsp;&nbsp;&nbsp;Recursos:X
+                    </div>
+                    <div className='Game-board-statics-name'>
+                        Desarrollos:X&nbsp;Caballeros:X
+                    </div>
+                    <div className='Game-board-statics-name'>
+                        Carreteras:X&nbsp;Puntos:X
+                    </div>
+                </div>     
+            </div>
+            <div className='Game-board-rec'>
+
+                
+
+                
+
+                
+
+                
+
+                
+  
+            </div>
+
+
         </div>
+        
     )
 }
 
